003ab69a8fa9c257da23e03b44aac20c63003e93
Alve Svarén <alve@hotmail.se>
2020-11-27 21:42:31 +0100

    Cleaner code

--- aoc/_api.py
+++ aoc/_api.py
@@ -5,40 +5,41 @@ from datetime import datetime
 _SESSION_FILE_NAME = "session.txt"
 _YEAR_FILE_NAME = "year.txt"
 
-try:
-    with open(_SESSION_FILE_NAME) as file:
-        SESSION = file.readline().strip()
-except FileNotFoundError:
-    with open(_SESSION_FILE_NAME, "w") as file:
-        SESSION = input("Enter your session cookie: ")
-        file.write(SESSION)
 
-try:
-    with open(_YEAR_FILE_NAME) as file:
-        YEAR = file.readline().strip()
-except FileNotFoundError:
-    with open(_YEAR_FILE_NAME, "w") as file:
-        YEAR = datetime.now().year
-        file.write(str(YEAR))
+def _set_read_file(filename: str, default: str = None) -> str:
+    try:
+        with open(filename) as file:
+            return file.read()
+    except FileNotFoundError:
+        if default:
+            with open(filename, "w") as file:
+                file.write(default)
+                return default
+        return None
+
+
+SESSION = _set_read_file(_SESSION_FILE_NAME)
+if not SESSION:
+    SESSION = _set_read_file(
+        _SESSION_FILE_NAME,
+        input("Enter your session cookie: "))
+
+YEAR = _set_read_file(_YEAR_FILE_NAME)
+if not YEAR:
+    YEAR = _set_read_file(
+        _YEAR_FILE_NAME,
+        str(datetime.now().year))
 
 
 def get_input(day: int, year: int = YEAR):
     Path("data").mkdir(exist_ok=True)
 
     file_name = f"data/{year}_{day}.txt"
-    try:
-        with open(file_name) as file:
-            return file.read()
-    except FileNotFoundError:
-        with open(file_name, "w") as file:
-            data = requests.get(
+    data = _set_read_file(file_name)
+    if not data:
+        data = _set_read_file(
+            file_name,
+            requests.get(
                 f"https://adventofcode.com/{year}/day/{day}/input",
-                cookies={"session": SESSION}
-            ).text[:-1]
-            file.write(data)
-            return data
-
-
-def set_year(year: int):
-    with open(_YEAR_FILE_NAME, "w") as file:
-        file.write(str(year))
+                cookies={"session": SESSION}).text[:-1])
+    return data
