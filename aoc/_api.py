029e1fa32a95896aef23e491b283964175f2c2fa
Alve Svarén <alve@hotmail.se>
2020-12-02 09:04:20 +0100

    Fixed formatting

--- aoc/_api.py
+++ aoc/_api.py
@@ -48,7 +48,9 @@ def get_input(day: int, year: int = YEAR, overwrite: bool = False):
     if overwrite:
         data = None
     if not data:
-        response = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies={"session": SESSION})
+        response = requests.get(
+            f"https://adventofcode.com/{year}/day/{day}/input",
+            cookies={"session": SESSION})
         if not response.ok:
             raise RuntimeError(f"Request failed, code: {response.status_code}, message: {response.content}")
         data = _set_read_file(
